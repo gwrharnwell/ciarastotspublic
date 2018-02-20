041db87b38f6c5d856cb21a9f5f14529bb50f599
gwrharnwell <georgeharnwell@googlemail.com>
2018-02-20 00:27:04 +0000

    Only render gallery on gallery pag

--- js/main.js
+++ js/main.js
@@ -151,7 +151,9 @@ $(".fancybox").fancybox({
     }
 });
 
-Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
-Galleria.run('.galleria');
+if ($('.galleria').length > 0) { 
+    Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js');
+    Galleria.run('.galleria');
+}
 
 
